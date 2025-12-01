3ebc7e8f686cd0dbe2821d3c909e860de3f1d5b1
Chen Liang <87241041+Chen2357@users.noreply.github.com>
2025-11-30 18:45:28 -0800

    Add `d_one`

--- Cochain/Cartan.lean
+++ Cochain/Cartan.lean
@@ -76,6 +76,8 @@ theorem ι_algebraMap (x : L) (f : M) :
   ι x (algebraMap M (Cochain A L M) f) = 0 := by
   simp [algebraMap_apply]
 
+section
+
 variable [LieRingModule L M] [LieRinehartModule A L M] [LieRinehartModule.IsTrivial A L M]
 
 @[simp]
@@ -91,6 +93,18 @@ theorem d_algebraMap_apply_one_apply (f : M) (v : Fin 1 → L) : d (algebraMap M
 theorem d_algebraMap_apply_ne (f : M) (n : ℕ) (hn : n ≠ 1) : d (algebraMap M (Cochain A L M) f) n = 0 := by
   simp [algebraMap_apply]
   rw [of_eq_of_ne _ _ _ hn]
+
+end
+
+variable [LieRinehartPair M L] [LieRinehartModule A L M] [LieRinehartModule.IsTrivial A L M]
+
+@[simp]
+theorem d_one : d 1 = (0 : Cochain A L M) := by
+  ext i v
+  by_cases h : i = 1
+  . cases h
+    simp [one_def]
+  . simp [one_def, of_eq_of_ne _ _ _ h]
 
 end
 
